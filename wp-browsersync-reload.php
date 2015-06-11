bf94f92b256b9a7bb5d64d1e9de0536e7a72f378
zach-adams <zach@zach-adams.com>
2015-06-11 17:08:04 -0600

    Initial commit

---
+++ wp-browsersync-reload.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	Plugin Name: WP Browsersync Reload
+	Plugin URI:  https://zach-adams.com
+	Description:
+	Version:     1.0
+	Author:      Zach Adams
+	Author URI:  https://zach-adams.com
+	License:     GPL2
+	License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
