5060e4de9b7a17f5a09d4cdeb827dcbbb8c4be61
zach-adams <zach@zach-adams.com>
2015-06-11 17:11:20 -0600

    Basic plugin structure

--- wp-browsersync-reload.php
+++ wp-browsersync-reload.php
@@ -9,3 +9,17 @@
 	License:     GPL2
 	License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
+
+class WP_Browsersync_Reload {
+	public static function init() {
+		$self = new self();
+		add_action('save_post', array($self, 'reload_browsersync'));
+	}
+
+	public function reload_browsersync() {
+		$args = ['blocking' => false];
+		wp_remote_get('http://192.168.1.2:3000/__browser_sync__?method=reload', $args);
+	}
+}
+
+add_action('plugins_loaded', array('WP_Browsersync_Reload', 'init'));
